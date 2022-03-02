ee8e45c4b1006c1936c834489d9c48b7b65ccf94
Arturo Gonzalez <Trkshgld@gmail.com>
2022-03-02 08:59:00 -0500

    first commit

---
+++ source/repos/Contact-List/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Contact_List
+{
+    class Program
+    {
+        static List<Person> People = new List<Person>();
+        Person person = new Person();
+        static void Main(string[] args)
+        {
+            
+        }
+    class Person
+        {
+            string? firstName { get; set; }
+            string? lastName { get; set; }
+            public string fullName => firstName + " " + lastName;
+            public string? phoneNumber { get; set; }
+            public string[]? Addresses { get; set; }
+            public static List<Person> People = new List<Person>();
+        }
+    }
+}
